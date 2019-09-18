d9ea21607773bf321bdd6fb13750921b0470257e
81254807 <bes.jira.notification@gmail.com>
2019-09-18 17:10:35 +0800

    menu details cleanup

--- src/app/pages/menu/menu-details/menu-details.component.ts
+++ src/app/pages/menu/menu-details/menu-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { MenuService } from '../menu.service';
 import { Subscription } from 'rxjs';
 import { Menu, Food } from '../menu.interface';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-menu-details',
@@ -14,6 +15,7 @@ export class MenuDetailsComponent implements OnInit, OnDestroy {
   subs: Subscription[] = [];
   menu: Menu;
   totalPrice = 0;
+  updatePriceInterval: any;
 
   private addons: {
     quantity: number,
@@ -28,8 +30,8 @@ export class MenuDetailsComponent implements OnInit, OnDestroy {
     this.subs.push(this.menuService.storeMenu().subscribe(x => {
       if (x.action === 'MENU_SELECT') {
         this.menu = x.menu.selected;
-        this.totalPrice = this.getTotalPrice();
       } else if (x.action === 'MENU_CLEAR_SELECTED') {
+        clearInterval(this.updatePriceInterval);
         this.menu = null;
         this.addons = [];
         this.totalPrice = 0;
@@ -38,17 +40,23 @@ export class MenuDetailsComponent implements OnInit, OnDestroy {
       const addonActions = ['MENU_ADD_ADDON', 'MENU_REMOVE_ADDON'];
       if (addonActions.indexOf(x.action) >= 0)  {
         this.addons = x.menu.addons;
-        this.totalPrice = this.getTotalPrice();
       }
     }));
+
+    this.updatePriceInterval = setInterval( x => {
+      this.totalPrice = this.getTotalPrice();
+    }, 100);
   }
 
   private getTotalPrice() {
     // add the default food with varians additional
     let additional = 0;
-    this.menu.foods.forEach( x => {
-      additional += (x.variant ? x.variant.additional : 0);
-    });
+    if (this.menu) {
+      this.menu.foods.forEach( x => {
+        additional += (x.variant ? x.variant.additional : 0);
+      });
+    }
+
 
     // Add addons on the total price
     let addonsTotalPrice = 0;
@@ -61,5 +69,6 @@ export class MenuDetailsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subs.forEach( x => x.unsubscribe());
+    clearInterval(this.updatePriceInterval);
   }
 }
--- src/app/pages/menu/menu-foods/menu-foods.component.ts
+++ src/app/pages/menu/menu-foods/menu-foods.component.ts
@@ -9,14 +9,33 @@ import { Food } from '../menu.interface';
 export class MenuFoodsComponent implements OnInit {
 
   @Input() foods: Food[];
+  foodIds: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.foodIds = this.getFoodIds(this.foods);
   }
 
   count(id: string) {
     return this.foods.filter( x => x._id === id ).length;
   }
 
+  // withou duplicate
+  private getFoodIds(foods: Food[]): string[] {
+    const idHolder = [];
+    const idHolderWithIndex = [];
+    foods.forEach( (x: Food, index: number) => {
+      const idWithIndex = `${index}-${x._id}`;
+      if (idHolder.indexOf(x._id) < 0) {
+        idHolder.push(x._id);
+        idHolderWithIndex.push(idWithIndex);
+      }
+    });
+    return idHolderWithIndex;
+  }
+
+  existOnTheFoodIds(id, index) {
+    return this.foodIds.indexOf(`${index}-${id}`) >= 0;
+  }
 }
